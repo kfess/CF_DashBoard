735b6247c1eb18105b5c6c33f7d2cc311c1ff79b
kita456 <g140702@gmail.com>
2023-01-14 22:45:47 +0900

    color を追加

---
+++ src/features/color/ratingColor.ts
@@ -0,0 +1,164 @@
+type RatingColor =
+  | "Black"
+  | "Gray"
+  | "Green"
+  | "Cyan"
+  | "Blue"
+  | "Violet"
+  | "LightOrange"
+  | "DeepOrange"
+  | "LightRed"
+  | "Red"
+  | "DeepRed";
+
+type ColorCode =
+  | "#000000" // Black
+  | "#CCCCCC" // Gray
+  | "#76FF77" // Cyan
+  | "#76DEBB" // Green
+  | "#AAAAFF" // Blue
+  | "#FF88FF" // Violet
+  | "#FFCC87" // LightOrange
+  | "#FFBB55" // DeepOrange
+  | "#FF7777" // LightRed
+  | "#FF3333" // Red
+  | "#AA0100"; // DeepRed
+
+type Title =
+  | "Legendary Grandmaster" // DeepRed
+  | "International Grandmaster" // Red
+  | "Grandmaster" // LightRed
+  | "International Master" // DeepOrange
+  | "Master" // LightOrange
+  | "Candidate Master" // Violet
+  | "Expert" // Blue
+  | "Specialist" // Cyan
+  | "Pupil" // Green
+  | "Newbie" // Gray
+  | "No Data"; // Black
+
+type RatingInfo = {
+  type: RatingColor;
+  colorCode: ColorCode;
+  lowerBound: number;
+  upperBound: number;
+  title: Title;
+};
+
+const MAX_RATING = 5000; // max rating of Codeforces?
+
+const blackRating: RatingInfo = {
+  type: "Black",
+  colorCode: "#000000",
+  lowerBound: -1,
+  upperBound: -1,
+  title: "No Data",
+};
+
+const grayRating: RatingInfo = {
+  type: "Gray",
+  colorCode: "#CCCCCC",
+  lowerBound: 0,
+  upperBound: 1199,
+  title: "Newbie",
+};
+
+const greenRating: RatingInfo = {
+  type: "Green",
+  colorCode: "#CCCCCC",
+  lowerBound: 1200,
+  upperBound: 1399,
+  title: "Pupil",
+};
+
+const cyanRating: RatingInfo = {
+  type: "Cyan",
+  colorCode: "#CCCCCC",
+  lowerBound: 1400,
+  upperBound: 1599,
+  title: "Specialist",
+};
+
+const blueRating: RatingInfo = {
+  type: "Blue",
+  colorCode: "#CCCCCC",
+  lowerBound: 1600,
+  upperBound: 1899,
+  title: "Expert",
+};
+
+const violetRating: RatingInfo = {
+  type: "Violet",
+  colorCode: "#CCCCCC",
+  lowerBound: 1900,
+  upperBound: 2099,
+  title: "Candidate Master",
+};
+
+const lightOrangeRating: RatingInfo = {
+  type: "LightOrange",
+  colorCode: "#CCCCCC",
+  lowerBound: 2100,
+  upperBound: 2299,
+  title: "Master",
+};
+
+const deepOrangeRating: RatingInfo = {
+  type: "DeepOrange",
+  colorCode: "#CCCCCC",
+  lowerBound: 2300,
+  upperBound: 2399,
+  title: "International Master",
+};
+
+const lightRedRating: RatingInfo = {
+  type: "LightRed",
+  colorCode: "#CCCCCC",
+  lowerBound: 2400,
+  upperBound: 2599,
+  title: "Grandmaster",
+};
+
+const redRating: RatingInfo = {
+  type: "Red",
+  colorCode: "#CCCCCC",
+  lowerBound: 2600,
+  upperBound: 2999,
+  title: "International Grandmaster",
+};
+
+const deepRedRating: RatingInfo = {
+  type: "DeepRed",
+  colorCode: "#CCCCCC",
+  lowerBound: 3000,
+  upperBound: MAX_RATING,
+  title: "Legendary Grandmaster",
+};
+
+export const getColorCodeFromRating = (
+  rating: number | undefined | undefined
+): ColorCode => {
+  if (rating === null || rating === undefined) {
+    return blackRating.colorCode;
+  } else if (rating <= grayRating.upperBound) {
+    return grayRating.colorCode;
+  } else if (rating <= greenRating.upperBound) {
+    return greenRating.colorCode;
+  } else if (rating <= cyanRating.upperBound) {
+    return cyanRating.colorCode;
+  } else if (rating <= blackRating.upperBound) {
+    return blueRating.colorCode;
+  } else if (rating <= violetRating.upperBound) {
+    return violetRating.colorCode;
+  } else if (rating <= lightOrangeRating.upperBound) {
+    return lightOrangeRating.colorCode;
+  } else if (rating <= deepOrangeRating.upperBound) {
+    return deepOrangeRating.colorCode;
+  } else if (rating <= lightRedRating.upperBound) {
+    return lightRedRating.colorCode;
+  } else if (rating <= redRating.upperBound) {
+    return redRating.colorCode;
+  } else {
+    return deepRedRating.colorCode;
+  }
+};
