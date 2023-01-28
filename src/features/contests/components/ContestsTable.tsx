95b02425fd6e0e7ee0d9f7251a88f01ebd5dec38
kita456 <g140702@gmail.com>
2023-01-28 11:44:38 +0900

    add contest pagination

--- src/features/contests/components/ContestsTable.tsx
+++ src/features/contests/components/ContestsTable.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
+import TablePagination from "@mui/material/TablePagination";
 import Paper from "@mui/material/Paper";
 import type { Contest } from "@features/contests/contest";
 import { ContestTableRow } from "@features/contests/components/ContestTableRow";
@@ -14,23 +15,62 @@ type Props = {
 
 export const ContestsTable: React.FC<Props> = (props: Props) => {
   const { contests, problemIdxes } = props;
+
+  const [page, setPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(50);
+  const contestsLen = useMemo(() => contests.length, [contests]);
+
+  const handleChangePage = (
+    _event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <TableHead>
-        <TableRow>
-          <TableCell>Contest</TableCell>
-          {problemIdxes.map((idx) => (
-            <TableCell>{idx}</TableCell>
+    <>
+      <TablePagination
+        component="div"
+        count={contestsLen}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+      <TableContainer component={Paper}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Contest</TableCell>
+            {problemIdxes.map((idx) => (
+              <TableCell>{idx}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        {[...contests]
+          .slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+          .map((contest) => (
+            <ContestTableRow
+              contestId={contest.id}
+              contestName={contest.name}
+              problems={contest.problems}
+            />
           ))}
-        </TableRow>
-      </TableHead>
-      {contests.map((contest) => (
-        <ContestTableRow
-          contestId={contest.id}
-          contestName={contest.name}
-          problems={contest.problems}
-        />
-      ))}
-    </TableContainer>
+      </TableContainer>
+      <TablePagination
+        component="div"
+        count={contestsLen}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
   );
 };
--- src/pages/contests/index.tsx
+++ src/pages/contests/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import { useFetchContests } from "@features/contests/useFetchContest";
 import { ContestsTable } from "@features/contests/components/ContestsTable";
 import type { Classification } from "@features/contests/contest";
@@ -8,6 +8,7 @@ import { FilterOptions } from "@features/contests/components/FilterOptions";
 export const ContestsPage: React.FC = () => {
   const { data, isError, error, isLoading } = useFetchContests();
   const [tab, setTab] = useState<Classification>("All");
+
   const contest = filterContest(data ?? [], tab);
   const problemIdxes = getProblemIdxes(contest);
 
