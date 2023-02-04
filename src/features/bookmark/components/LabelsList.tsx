e779541bf2e14c8a9a0a30049b6a46120f330ac2
kita456 <g140702@gmail.com>
2023-02-04 23:26:22 +0900

    add sort labels

--- src/features/bookmark/components/LabelsList.tsx
+++ src/features/bookmark/components/LabelsList.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import CheckIcon from "@mui/icons-material/Check";
 import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { useRecoilState } from "recoil";
 import { labelsState } from "@features/bookmark/label.atom";
 import { LabelNameChip } from "./LabelIcon";
+
+const sortOrders = [
+  "Alphabetically",
+  "Reverse Alphabetically",
+  "Most Problems",
+  "Fewest Problems",
+] as const;
+type SortOrder = typeof sortOrders[number];
 
 export const LabelsList: React.FC = () => {
   const [labels, setLabels] = useRecoilState(labelsState);
@@ -15,6 +28,29 @@ export const LabelsList: React.FC = () => {
   const deleteLabel = (id: number) => {
     setLabels(labels.filter((label) => label.id !== id));
   };
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const [order, setOrder] = useState<SortOrder>("Alphabetically");
+  const sortedLabels = [...labels].sort((a, b) => {
+    switch (order) {
+      case "Alphabetically":
+        return a.name.localeCompare(b.name);
+      case "Reverse Alphabetically":
+        return b.name.localeCompare(a.name);
+      case "Most Problems":
+        return b.problems.length - a.problems.length;
+      case "Fewest Problems":
+        return a.problems.length - b.problems.length;
+    }
+  });
 
   return (
     <>
@@ -29,9 +65,31 @@ export const LabelsList: React.FC = () => {
             }}
           >
             <Box>{labels.length} labels</Box>
-            <Box>sort</Box>
+            <Box>
+              <Button
+                endIcon={
+                  open ? <KeyboardArrowDownIcon /> : <KeyboardArrowLeftIcon />
+                }
+                onClick={handleClick}
+              >
+                sort
+              </Button>
+              <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+                {sortOrders.map((sortOrder) => (
+                  <MenuItem
+                    onClick={() => {
+                      setOrder(sortOrder);
+                      setAnchorEl(null);
+                    }}
+                  >
+                    {sortOrder === order && <CheckIcon fontSize="small" />}
+                    {sortOrder}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
           </Box>
-          {labels.map((label) => (
+          {sortedLabels.map((label) => (
             <Box sx={{ p: 1, m: 1, display: "flex" }}>
               <Box sx={{ width: "25%", textAlign: "left" }}>
                 <LabelNameChip
