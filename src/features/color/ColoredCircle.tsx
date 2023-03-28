9edc8ffadc6dabf8ace5d5cb1da57849e256ec9b
kita456 <g140702@gmail.com>
2023-03-28 11:51:16 +0900

    add test

--- src/features/color/ColoredCircle.tsx
+++ src/features/color/ColoredCircle.tsx
@@ -1,39 +1,43 @@
 import React from "react";
-import { css } from "@emotion/react";
+import { styled } from "@mui/system";
 
-const circle = css({
+const StyledCircle = styled("span", {
+  shouldForwardProp: (prop) => prop !== "color" && prop !== "fillPercent",
+})<{
+  color: string;
+  fillPercent?: number;
+  width?: string;
+  height?: string;
+}>(({ theme, color, fillPercent = 1, width, height }) => ({
   borderStyle: "solid",
   borderWidth: "thin",
   display: "inline-block",
   borderRadius: "50%",
   padding: "5px",
   margin: "0 5px",
-});
+  width: width,
+  height: height,
+  borderColor: color,
+  background: `linear-gradient(to top, ${color} ${
+    (fillPercent ?? 1) * 100
+  }%, rgba(0,0,0,0) ${(fillPercent ?? 1) * 100}%)`,
+}));
 
 type Props = {
-  color: string;
-  fillPercent?: number;
-  width?: string;
-  height?: string;
+  readonly color: string;
+  readonly fillPercent?: number;
+  readonly width?: string;
+  readonly height?: string;
 };
 
 export const ColoredCircle: React.FC<Props> = (props: Props) => {
-  const { color, fillPercent = 1, width, height } = props;
-
   return (
-    <span
+    <StyledCircle
       aria-label="colored circle"
-      css={[
-        circle,
-        {
-          width: width,
-          height: height,
-          borderColor: color,
-          background: `border-box linear-gradient(to top,
-       ${color} ${(fillPercent ?? 1) * 100}%,
-       rgba(0,0,0,0) ${(fillPercent ?? 1) * 100}%)`,
-        },
-      ]}
+      color={props.color}
+      fillPercent={props.fillPercent}
+      width={props.width}
+      height={props.height}
     />
   );
 };
--- src/features/color/components/__tests__/ColoredCircle.test.tsx
+++ src/features/color/components/__tests__/ColoredCircle.test.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-
+import "@testing-library/jest-dom/extend-expect";
 import { ColoredCircle } from "@features/color/ColoredCircle";
 
 describe("ColoredCircle", () => {
   test("renders the circle with the specified color", () => {
     render(<ColoredCircle color="#FF0000" />);
     const circleElement = screen.getByLabelText("colored circle");
-    expect(circleElement).toHaveStyle({ borderColor: "#FF0000" });
+    expect(circleElement).toHaveStyle({
+      borderColor: "#FF0000",
+    });
   });
 });
