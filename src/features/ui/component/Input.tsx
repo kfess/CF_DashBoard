c4f1648594d1333b49abb1cdb0fd631ee5c17b23
kita456 <g140702@gmail.com>
2023-04-01 15:51:40 +0900

    fix input

--- src/features/ui/component/Input.tsx
+++ src/features/ui/component/Input.tsx
@@ -1,38 +1,65 @@
-import { alpha, styled } from "@mui/material/styles";
-import InputBase from "@mui/material/InputBase";
+import * as React from "react";
+import { InputUnstyledProps } from "@mui/base/InputUnstyled";
+import InputUnstyled from "@mui/base/InputUnstyled";
+import { styled } from "@mui/system";
 
-export const Input = styled(InputBase)(({ theme }) => ({
-  "label + &": {
-    marginTop: theme.spacing(3),
-  },
-  "& .MuiInputBase-input": {
-    borderRadius: 4,
-    position: "relative",
-    backgroundColor: theme.palette.mode === "light" ? "#fcfcfb" : "#2b2b2b",
-    border: "1px solid #ced4da",
-    fontSize: 16,
-    padding: "4px 10px",
-    transition: theme.transitions.create([
-      "border-color",
-      "background-color",
-      "box-shadow",
-    ]),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-    "&:focus": {
-      boxShadow: `${alpha(theme.palette.primary.main, 0.8)} 0 0 0 0.1rem`,
-      borderColor: "#58A6FF",
-    },
-  },
-}));
+const blue = {
+  100: "#DAECFF",
+  200: "#80BFFF",
+  400: "#3399FF",
+  500: "#007FFF",
+  600: "#0072E5",
+};
+
+const grey = {
+  50: "#F3F6F9",
+  100: "#E7EBF0",
+  200: "#E0E3E7",
+  300: "#CDD2D7",
+  400: "#B2BAC2",
+  500: "#A0AAB4",
+  600: "#6F7E8C",
+  700: "#3E5060",
+  800: "#2D3843",
+  900: "#1A2027",
+};
+
+const StyledInputElement = styled("input")(
+  ({ theme }) => `
+  width: 100%;
+  font-size: 0.875rem;
+  padding: 0.4rem;
+  border-radius: 0.375rem;
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+  box-shadow: 0px 2px 2px ${
+    theme.palette.mode === "dark" ? grey[900] : grey[50]
+  };
+
+  &:hover {
+    border-color: ${blue[400]};
+  }
+
+  &:focus {
+    border-color: ${blue[600]};
+    box-shadow: 0 0 0 2px ${
+      theme.palette.mode === "dark" ? blue[500] : blue[400]
+    };
+  }
+
+  // firefox
+  &:focus-visible {
+    outline: 0;
+  }
+`
+);
+
+export const Input = React.forwardRef(function CustomInput(
+  props: InputUnstyledProps,
+  ref: React.ForwardedRef<HTMLDivElement>
+) {
+  return (
+    <InputUnstyled slots={{ input: StyledInputElement }} {...props} ref={ref} />
+  );
+});
