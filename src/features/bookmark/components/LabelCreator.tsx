faaab636f927510c20ee109ed2b67a7e04bd6b16
kita456 <g140702@gmail.com>
2023-05-04 00:30:22 +0900

    fix label creator

--- src/features/bookmark/components/LabelCreator.tsx
+++ src/features/bookmark/components/LabelCreator.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
+import { Button } from "@features/ui/component/Button";
 import InputLabel from "@mui/material/InputLabel";
 import IconButton from "@mui/material/IconButton";
 import ReplayIcon from "@mui/icons-material/Replay";
@@ -40,29 +39,29 @@ export const LabelCreator: React.FC = () => {
   return (
     <>
       <Box sx={{ p: 1, display: "flex", justifyContent: "flex-end" }}>
-        <Button
-          onClick={toggleShowBlock}
-          variant="contained"
-          color="primary"
-          size="small"
-          css={{ textTransform: "none" }}
-        >
-          New Label
-        </Button>
+        <Button onClick={toggleShowBlock}>New Label</Button>
       </Box>
       {showBlock && (
-        <>
+        <Box
+          sx={{
+            border: "1px solid",
+            borderColor: "divider",
+            borderRadius: "6px",
+            backgroundColor: "white",
+            p: 2,
+          }}
+        >
           <LabelNameChip
             name={name.value}
             color={isValidHexaColor(color) ? color : "#000000"}
             mode="Preview"
           />
-          <Box component="form" autoComplete="off" sx={{ display: "flex" }}>
+          <Box component="form" autoComplete="off">
             <Stack
-              direction={{ xs: "column", sm: "row" }}
-              spacing={{ xs: 1, sm: 2, md: 4 }}
+              direction={{ xs: "column", sm: "row", md: "row" }}
+              sx={{ width: "100%" }}
             >
-              <Box sx={{ p: 1 }}>
+              <Box sx={{ p: 1, flexGrow: 1, width: "100%" }}>
                 <InputLabel
                   css={{ paddingBottom: "0.3rem", fontWeight: "bold" }}
                 >
@@ -76,7 +75,7 @@ export const LabelCreator: React.FC = () => {
                   placeholder="label name"
                 />
               </Box>
-              <Box sx={{ p: 1 }}>
+              <Box sx={{ p: 1, flexGrow: 1, width: "100%" }}>
                 <InputLabel
                   css={{ paddingBottom: "0.3rem", fontWeight: "bold" }}
                 >
@@ -85,70 +84,79 @@ export const LabelCreator: React.FC = () => {
                 <Input
                   value={description.value}
                   onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                    setName({ value: event.target.value, errorMsg: "" })
+                    setDescription({ value: event.target.value, errorMsg: "" })
                   }
-                  placeholder="description"
+                  placeholder="description (optional)"
                 />
               </Box>
-              <Box sx={{ p: 1 }}>
-                <InputLabel css={{ fontWeight: "bold" }}>
+              <Box
+                sx={{
+                  p: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  flexGrow: 1,
+                  width: "100%",
+                }}
+              >
+                <InputLabel
+                  css={{ paddingBottom: "0.3rem", fontWeight: "bold" }}
+                >
                   Generate Color
                 </InputLabel>
-                <IconButton
-                  aria-label="generate"
-                  onClick={() => {
-                    setColor(generateRandomHexaColor());
-                  }}
-                >
-                  <ReplayIcon
-                    fontSize="inherit"
-                    css={{ color: isValidHexaColor(color) ? color : "#000000" }}
-                  />
-                </IconButton>
-                <Tooltip
-                  title={<ColorPalette setColor={setColor} />}
-                  arrow
-                  leaveDelay={300} // ms
-                >
-                  <Input
-                    value={color}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                      setColor(event.target.value as HexaColor)
-                    }
-                    css={{
-                      "& .MuiInputBase-root": {
-                        color: isValidHexaColor(color) ? color : "#000000",
-                      },
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <IconButton
+                    aria-label="generate"
+                    onClick={() => {
+                      setColor(generateRandomHexaColor());
                     }}
-                  />
-                </Tooltip>
+                  >
+                    <ReplayIcon
+                      fontSize="inherit"
+                      css={{
+                        color: isValidHexaColor(color) ? color : "#000000",
+                      }}
+                    />
+                  </IconButton>
+                  <Box sx={{ flexGrow: 1, width: "100%" }}>
+                    <Tooltip
+                      title={<ColorPalette setColor={setColor} />}
+                      arrow
+                      leaveDelay={300} // ms
+                    >
+                      <Input
+                        value={color}
+                        onChange={(
+                          event: React.ChangeEvent<HTMLInputElement>
+                        ) => setColor(event.target.value as HexaColor)}
+                        css={{
+                          "& .MuiInputBase-root": {
+                            color: isValidHexaColor(color) ? color : "#000000",
+                          },
+                        }}
+                      />
+                    </Tooltip>
+                  </Box>
+                </Box>
               </Box>
             </Stack>
           </Box>
-          <Box sx={{ p: 1, display: "flex", justifyContent: "flex-end" }}>
-            <ButtonGroup>
-              <Button
-                onClick={toggleShowBlock}
-                variant="contained"
-                color="secondary"
-                size="small"
-                css={{ textTransform: "none" }}
-              >
-                Cancel
-              </Button>
-              <Button
-                onClick={onClickCreateLabel}
-                disabled={name.value.length === 0 || !isValidHexaColor(color)}
-                variant="contained"
-                color="success"
-                size="small"
-                css={{ textTransform: "none" }}
-              >
-                Create Label
-              </Button>
-            </ButtonGroup>
+          <Box
+            sx={{
+              p: 1,
+              display: "flex",
+              justifyContent: "flex-end",
+              flexWrap: { xs: "wrap", sm: "nowrap" },
+            }}
+          >
+            <Button onClick={toggleShowBlock}>Cancel</Button>
+            <Button
+              onClick={onClickCreateLabel}
+              disabled={name.value.length === 0 || !isValidHexaColor(color)}
+            >
+              Create Label
+            </Button>
           </Box>
-        </>
+        </Box>
       )}
     </>
   );
--- src/features/ui/component/Input.tsx
+++ src/features/ui/component/Input.tsx
@@ -28,38 +28,69 @@ export type InputProps = InputUnstyledProps & {
   inputHeight?: string;
 };
 
-const StyledInputElement = styled("input")(
-  ({ theme }) => `
-  height: 23px;
-  width: 100%;
-  font-size: 1rem;
-  padding: 0.4rem;
-  padding-left:0.875rem;
-  border-radius: 0.375rem;
-  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
-  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
-  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
-  box-shadow: 0px 2px 2px ${
-    theme.palette.mode === "dark" ? grey[900] : grey[50]
-  };
+// const StyledInputElement = styled("input")(
+//   ({ theme }) => `
+//   line-height: 1.5;
+//   min-width: 100%;
+//   max-width: 100%;
+//   width: 100%;
+//   font-size: 1rem;
+//   padding: 0.4rem;
+//   padding-left:0.875rem;
+//   border-radius: 0.375rem;
+//   color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+//   background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+//   border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+//   box-shadow: 0px 2px 2px ${
+//     theme.palette.mode === "dark" ? grey[900] : grey[50]
+//   };
 
-  &:hover {
-    border-color: ${blue[400]};
-  }
+//   &:hover {
+//     border-color: ${blue[400]};
+//   }
 
-  &:focus {
-    border-color: ${blue[600]};
-    box-shadow: 0 0 0 2px ${
-      theme.palette.mode === "dark" ? blue[500] : blue[400]
-    };
-  }
+//   &:focus {
+//     border-color: ${blue[600]};
+//     box-shadow: 0 0 0 2px ${
+//       theme.palette.mode === "dark" ? blue[500] : blue[400]
+//     };
+//   }
 
-  // firefox
-  &:focus-visible {
-    outline: 0;
-  }
-`
-);
+//   // firefox
+//   &:focus-visible {
+//     outline: 0;
+//   }
+// `
+// );
+
+import InputBase from "@mui/material/InputBase";
+const StyledInputElement = styled(InputBase)(({ theme }) => ({
+  boxSizing: "border-box",
+  lineHeight: "1",
+  fontSize: "1rem",
+  borderRadius: theme.shape.borderRadius,
+  border: `1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]}`,
+  backgroundColor:
+    theme.palette.mode === "dark" ? theme.palette.grey[900] : "#fff",
+  "&.Mui-focused": {
+    borderColor: theme.palette.primary.main,
+    boxShadow: `0 0 0 0.2rem ${theme.palette.primary.light}`,
+  },
+  "&.Mui-error": {
+    borderColor: theme.palette.error.main,
+    "&:hover": {
+      borderColor: theme.palette.error.main,
+    },
+  },
+  "& input": {
+    padding: theme.spacing(1, 1.2),
+    minHeight: "23px",
+    height: "100%",
+    boxSizing: "border-box",
+  },
+  width: "100%",
+  flexGrow: 1,
+}));
 
 export const Input = React.forwardRef(function CustomInput(
   props: InputUnstyledProps,
