c754761f1203808ad572636b4db2c33d21c92fd6
kita456 <g140702@gmail.com>
2023-05-03 18:36:27 +0900

    add router when logged in

--- src/pages/authentication/login/index.tsx
+++ src/pages/authentication/login/index.tsx
@@ -1,27 +1,40 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import { Google, Facebook, Twitter } from "@mui/icons-material";
-import { SignInOutButton } from "@features/authentication/components/SignInOut";
+import { useLoggedIn } from "@features/authentication/hooks/useLoggedIn";
+import { SocialLoginButton } from "@features/authentication/components/SocialLoginButton";
 
 export const LoginPage: React.FC = () => {
+  const { loggedIn } = useLoggedIn();
+  const navigate = useNavigate();
+
+  if (loggedIn) {
+    navigate("/");
+  }
+
   return (
     <Grid
       container
       justifyContent="center"
       alignItems="center"
-      minHeight="100vh"
+      minHeight="50vh"
       spacing={2}
     >
       <Grid item xs={12} sm={8} md={6} lg={4}>
         <Card>
           <CardContent>
-            <Typography variant="h5" gutterBottom>
-              Sign in with social account
+            <Typography variant="h5" align="center">
+              CF Dashboard
             </Typography>
-            <SignInOutButton />
+          </CardContent>
+          <CardContent>
+            <Typography variant="h6" gutterBottom>
+              Sign in with
+            </Typography>
+            <SocialLoginButton provider="github" />
           </CardContent>
         </Card>
       </Grid>
