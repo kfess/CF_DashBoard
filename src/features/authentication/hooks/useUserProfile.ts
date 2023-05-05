162dbc28f85ea54717246208fa2e4bb3c464514b
kita456 <g140702@gmail.com>
2023-05-05 21:29:06 +0900

    add misc

--- src/features/authentication/hooks/useUserProfile.ts
+++ src/features/authentication/hooks/useUserProfile.ts
@@ -4,12 +4,9 @@ import { CF_USER_INFO_URL } from "@constants/url";
 import { okUserInfoApiSchema } from "@features/layout/userInfo";
 import { userProfileSchema } from "@features/authentication/userProfile";
 import type { UserProfile } from "@features/authentication/userProfile";
-// import type { CustomContest } from "@features/custom_contests/customContest";
 
 const INTERNAL_CF_GET_URL = "http://localhost:4000/api/users/find";
 const INTERNAL_CF_UPDATE_URL = "http://localhost:4000/api/users/update";
-// const INTERNAL_ADD_OWNED_CONTEST_URL = "/mock/user/ownedContests/add";
-// const INTERNAL_ADD_JOINED_CONTEST_URL = "/mock/user/joinedContests/add";
 
 const fetchUserProfile = async (): Promise<UserProfile> => {
   try {
@@ -57,26 +54,6 @@ const updateCodeforcesUsernameIfExists = async (
   }
 };
 
-// const addJoinedCustomContest = async (
-//   contest: CustomContest
-// ): Promise<void> => {
-//   try {
-//     await axios.post(INTERNAL_ADD_JOINED_CONTEST_URL, contest);
-//   } catch (error) {
-//     throw new Error(
-//       "An error occurred while adding the joined custom contest."
-//     );
-//   }
-// };
-
-// const addOwnedCustomContest = async (contest: CustomContest): Promise<void> => {
-//   try {
-//     await axios.post(INTERNAL_ADD_OWNED_CONTEST_URL, contest);
-//   } catch (error) {
-//     throw new Error("An error occurred while adding the owned custom contest.");
-//   }
-// };
-
 export const useUserProfile = () => {
   const queryClient = useQueryClient();
 
@@ -103,34 +80,6 @@ export const useUserProfile = () => {
     }
   );
 
-  // // mutate ownedCustomContests
-  // const addOwnedContestMutation = useMutation<void, Error, CustomContest>(
-  //   (contest: CustomContest) => addOwnedCustomContest(contest),
-  //   {
-  //     onSuccess: () => {
-  //       queryClient.invalidateQueries(["userProfile"]); // update contests immediately
-  //     },
-  //     onError: (error) => {
-  //       console.log("An error occurred during adding owned custom contest");
-  //       // add Toast in the future
-  //     },
-  //   }
-  // );
-
-  // // mutate joinedCustomContests
-  // const addJoinedContestMutation = useMutation<void, Error, CustomContest>(
-  //   (contest: CustomContest) => addJoinedCustomContest(contest),
-  //   {
-  //     onSuccess: () => {
-  //       queryClient.invalidateQueries(["userProfile"]); // update contests immediately
-  //     },
-  //     onError: (error) => {
-  //       console.log("An error occurred during adding joined custom contest");
-  //       // add Toast in the future
-  //     },
-  //   }
-  // );
-
   const updateUsername = async (codeforcesUsername: string) => {
     const pattern = /^[a-zA-Z0-9_]+$/;
     const isValidUsername = pattern.test(codeforcesUsername);
@@ -139,20 +88,10 @@ export const useUserProfile = () => {
     }
   };
 
-  // const addOwnedContest = async (contest: CustomContest) => {
-  //   await addOwnedContestMutation.mutateAsync(contest);
-  // };
-
-  // const addJoinedContest = async (contest: CustomContest) => {
-  //   await addJoinedContestMutation.mutateAsync(contest);
-  // };
-
   return {
     githubId: data?.githubId,
     githubUserName: data?.githubUsername,
     codeforcesUsername: data?.codeforcesUsername,
     updateUsername,
-    // addOwnedContest,
-    // addJoinedContest,
   };
 };
--- src/features/authentication/userProfile.ts
+++ src/features/authentication/userProfile.ts
@@ -1,5 +1,4 @@
 import { z } from "zod";
-import { customContestSchema } from "@features/custom_contests/customContest";
 
 export const userProfileSchema = z.object({
   githubId: z.number(),
@@ -8,16 +7,4 @@ export const userProfileSchema = z.object({
   isLoggedIn: z.boolean(),
 });
 
-export const userCustomContestSchema = z.object({
-  ownedCustomContests: z.array(customContestSchema),
-  joinedCustomContests: z.array(customContestSchema),
-});
-
-export const userProfileWithCustomContestSchema = userProfileSchema.merge(
-  userCustomContestSchema
-);
-
 export type UserProfile = z.infer<typeof userProfileSchema>;
-export type UserProfileWithCustomContestSchema = z.infer<
-  typeof userProfileWithCustomContestSchema
->;
