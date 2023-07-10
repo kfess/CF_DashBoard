4ecfbe87ed9d832c18d3e817d20e09987f94bfdf
kita456 <g140702@gmail.com>
2023-07-10 23:30:40 +0900

    add tooptip for solved status button

--- src/features/problems/components/SolvedStatusFilter.tsx
+++ src/features/problems/components/SolvedStatusFilter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Tooltip from "@mui/material/Tooltip";
 import { DropDownMenuButton } from "@features/ui/component/DropDownMenuButton";
 import { useQueryParams, QueryParamKeys } from "@hooks/useQueryParams";
 
@@ -20,14 +21,31 @@ export const SolvedStatusFilter: React.FC<Props> = (props: Props) => {
   const searchUserId = useQueryParams(QueryParamKeys.USERID);
 
   return (
-    <DropDownMenuButton
-      title="Solved Status"
-      items={solvedStatuses.map((ss) => {
-        return { item: ss };
-      })}
-      selectedItem={solvedStatus}
-      setSelectedItem={setSolvedStatus}
-      disabled={!searchUserId}
-    />
+    <>
+      {searchUserId ? (
+        <DropDownMenuButton
+          title="Solved Status"
+          items={solvedStatuses.map((ss) => {
+            return { item: ss };
+          })}
+          selectedItem={solvedStatus}
+          setSelectedItem={setSolvedStatus}
+        />
+      ) : (
+        <Tooltip title="To enable this Solved Status button, you need to enter the user name.">
+          <div>
+            <DropDownMenuButton
+              title="Solved Status"
+              items={solvedStatuses.map((ss) => {
+                return { item: ss };
+              })}
+              selectedItem={solvedStatus}
+              setSelectedItem={setSolvedStatus}
+              disabled={!searchUserId}
+            />
+          </div>
+        </Tooltip>
+      )}
+    </>
   );
 };
