bda755de5dd7b478ea3b8607ec3978b376b7b03e
kita456 <g140702@gmail.com>
2023-07-12 23:21:02 +0900

    remove unnecessaary code

--- src/features/achievement/components/TotalRatingSum.tsx
+++ src/features/achievement/components/TotalRatingSum.tsx
@@ -1,35 +1,3 @@
-// import React from "react";
-// import Stack from "@mui/material/Stack";
-// import type { Submission } from "@features/submission/submission";
-// import {
-//   isACSubmission,
-//   filterUniqueSubmissions,
-//   sumSubmissionsRating,
-// } from "@features/achievement/processSubmission";
-
-// type Props = { submissions: Submission[] };
-
-// export const TotalRatingSum: React.FC<Props> = (props: Props) => {
-//   const { submissions } = props;
-//   const ACSubmissions = submissions.filter(isACSubmission);
-//   const uniqueACSubmissions = filterUniqueSubmissions(ACSubmissions);
-//   const sum = sumSubmissionsRating(uniqueACSubmissions) ?? 0;
-
-//   return (
-//     <>
-//       <div>
-//         <strong>Rated Point Sum</strong>
-//       </div>
-//       <Stack direction="row" sx={{ display: "flex", m: 1 }}>
-//         <div>
-//           <strong>{sum.toLocaleString()}</strong>{" "}
-//           <span css={{ fontSize: "14px", color: "gray" }}>points</span>
-//         </div>
-//       </Stack>
-//     </>
-//   );
-// };
-
 import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
