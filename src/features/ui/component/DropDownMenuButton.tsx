f89394cdcdcfd8dc1d211dd74fe698cd497fde29
kita456 <g140702@gmail.com>
2023-08-26 22:13:54 +0900

    remove margintop

--- src/features/ui/component/DropDownMenuButton.tsx
+++ src/features/ui/component/DropDownMenuButton.tsx
@@ -1,88 +1,3 @@
-// import React, { ReactNode, useState } from "react";
-// import Menu from "@mui/material/Menu";
-// import MenuItem from "@mui/material/MenuItem";
-// import CheckIcon from "@mui/icons-material/Check";
-// import SvgIcon from "@mui/material/SvgIcon";
-// import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
-// import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-// import { Button } from "@features/ui/component/Button";
-
-// const ITEM_HEIGHT = 48;
-
-// type Props<T extends string | number> = {
-//   readonly title: string;
-//   selectedItem: T;
-//   setSelectedItem: (item: T) => void;
-//   readonly items: { readonly item: T; startIcon?: ReactNode }[];
-//   disabled?: boolean;
-// };
-
-// export const DropDownMenuButton = <T extends string | number>(
-//   props: Props<T>
-// ) => {
-//   const {
-//     title,
-//     selectedItem,
-//     setSelectedItem,
-//     items,
-//     disabled = false,
-//   } = props;
-
-//   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-//   const open = Boolean(anchorEl);
-
-//   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-//   const handleClose = (_event: React.MouseEvent<HTMLElement>) => {
-//     setAnchorEl(null);
-//   };
-
-//   return (
-//     <>
-//       <Button
-//         onClick={handleClick}
-//         endIcon={open ? <ArrowDropDownIcon /> : <ArrowLeftIcon />}
-//         disabled={disabled}
-//       >
-//         {title}
-//       </Button>
-//       <Menu
-//         open={open}
-//         anchorEl={anchorEl}
-//         anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-//         transformOrigin={{
-//           vertical: "top",
-//           horizontal: "left",
-//         }}
-//         onClose={handleClose}
-//         PaperProps={{
-//           style: { maxHeight: ITEM_HEIGHT * 4.5, marginTop: "8px" },
-//         }}
-//       >
-//         {items.map((item) => (
-//           <MenuItem
-//             key={item.item}
-//             onClick={() => {
-//               setSelectedItem(item.item);
-//               setAnchorEl(null);
-//             }}
-//             dense
-//           >
-//             {selectedItem === item.item ? (
-//               <CheckIcon fontSize="small" css={{ paddingRight: "5px" }} />
-//             ) : (
-//               <SvgIcon fontSize="small" css={{ paddingRight: "5px" }} />
-//             )}
-//             {item.startIcon}
-//             {item.item}
-//           </MenuItem>
-//         ))}
-//       </Menu>
-//     </>
-//   );
-// };
-
 import React, { ReactNode, useState } from "react";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
@@ -148,7 +63,7 @@ export const DropDownMenuButton = <T extends string | number>(
         }}
         onClose={handleClose}
         PaperProps={{
-          style: { maxHeight: ITEM_HEIGHT * 4.5, marginTop: "8px" },
+          style: { maxHeight: ITEM_HEIGHT * 4.5 },
         }}
       >
         {items.map((item) => (
