380bddca3cddaca2744faef2139b7e93ef52be79
kita456 <g140702@gmail.com>
2023-08-27 16:52:49 +0900

    add getProblemKey

--- src/features/problems/utils.ts
+++ src/features/problems/utils.ts
@@ -5,7 +5,6 @@ import type { Problem } from "@features/problems/problem";
 export function getProblemKey(submissions: Submission): string;
 export function getProblemKey(problem: Problem): string;
 
-// 実装
 // To get the unique key of a problem
 export function getProblemKey(args: Problem | Submission): string {
   const { contestId, index, name } = "problem" in args ? args.problem : args;
