3f34a0a5a17860af583a83c4fbb4a6df618dd4f9
kita456 <g140702@gmail.com>
2023-09-30 16:05:39 +0900

    add

--- src/features/setting/components/__test__/TimeZone.test.tsx
+++ src/features/setting/components/__test__/TimeZone.test.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
-import dayjs from "dayjs";
 import { TimeZone } from "../TimeZone";
 
+jest.mock("dayjs", () => {
+  const originalDayjs = jest.requireActual("dayjs");
+  originalDayjs.tz = {
+    ...originalDayjs.tz,
+    guess: jest.fn(() => "Asia/Tokyo"), // 任意のタイムゾーンを返すように設定
+  };
+  return originalDayjs;
+});
+
 describe("TimeZone Component", () => {
-  it("sould renders correctly", () => {
+  it("should renders correctly", () => {
     const { getByLabelText } = render(<TimeZone />);
     expect("Time Zone").toBeInTheDocument();
   });
