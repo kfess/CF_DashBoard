30fb1096021063dd02fc113119ff8bdff2610386
kita456 <g140702@gmail.com>
2023-10-01 20:34:01 +0900

    update document

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -6,20 +6,20 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "CF-DashBoard User Guide",
+  tagline: "Manage your Codeforces Problems",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
   url: "https://kfess.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/cf-dashboard",
+  baseUrl: "/CF_DashBoard",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "kfess", // Usually your GitHub org/user name.
+  projectName: "CF_DashBoard", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
